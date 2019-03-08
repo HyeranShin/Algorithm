43278e41552fc813cc0d316a9cb363513ae8c462
신혜란 <hyebaragi__@naver.com>
2019-03-08 13:51:45 +0900

    Create 6. 2440_별 찍기 - 3.cpp

---
+++ 3. for문 사용해보기/6. 2440_별 찍기 - 3.cpp
@@ -0,0 +1,23 @@
+/*
+첫째 줄에는 별 N개, 둘째 줄에는 별 N-1개, ..., N번째 줄에는 별 1개를 찍는 문제
+*/
+
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+ 
+int main(){
+ 
+	int N;
+	cin >> N;
+
+	for (int i = N; i >= 1; i--) {
+		for (int j = 1; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+  return 0;
+}
