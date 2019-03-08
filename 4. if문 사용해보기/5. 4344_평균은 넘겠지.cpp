f6113fc4c4876f35b645cb5b34fcd7c27e4994a7
신혜란 <hyebaragi__@naver.com>
2019-03-08 15:42:03 +0900

    Create 5. 4344_평균은 넘겠지.cpp

---
+++ 4. if문 사용해보기/5. 4344_평균은 넘겠지.cpp
@@ -0,0 +1,66 @@
+/*
+<문제>
+대학생 새내기들의 90%는 자신이 반에서 평균은 넘는다고 생각한다. 당신은 그들에게 슬픈 진실을 알려줘야 한다.
+
+<입력>
+첫째 줄에는 테스트 케이스의 개수 C가 주어진다.
+둘째 줄부터 각 테스트 케이스마다 학생의 수 N(1 ≤ N ≤ 1000, N은 정수)이 첫 수로 주어지고, 이어서 N명의 점수가 주어진다. 
+점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+
+<출력>
+각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력한다.
+*/
+
+#include<iostream>
+#include<ios>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+ 
+int main() {
+
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int N;
+
+	cin >> N;
+
+	vector<int> score;
+	vector<double> percentage;
+
+	for (int i = 0; i < N; i++) {
+		int personCnt;
+		float sum = 0;
+		int overAvgPsn = 0;
+
+		cin >> personCnt;
+		
+		for (int i = 0; i < personCnt; i++) {
+			int num;
+			cin >> num;
+			score.push_back(num);
+			sum += num;
+		}
+
+		int average = sum / personCnt;
+
+		for (int i = 0; i < personCnt; i++) {
+			if (score[i] > average) {
+				overAvgPsn++;
+			}
+		}
+
+		percentage.push_back((float)overAvgPsn / (float)personCnt * (float)100);
+
+		score.clear();
+	}
+	
+	for (int i = 0; i < N; i++) {
+		cout.precision(3);	// 소수 셋째자리까지 출력 함수
+		cout << fixed << percentage[i] << "%\n";	// 최종 출력 부분 ***fixed
+	}
+
+  return 0;
+}
