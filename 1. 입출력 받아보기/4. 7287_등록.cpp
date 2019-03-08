49904fbb74e2e73a30d65c06aaae90cd46c7dd75
신혜란 <hyebaragi__@naver.com>
2019-03-08 13:19:18 +0900

    Create 4. 7287_등록.cpp

---
+++ 1. 입출력 받아보기/4. 7287_등록.cpp
@@ -0,0 +1,16 @@
+/*
+자신이 온라인 저지에서 맞은 문제의 개수와 아이디를 그대로 출력하는 프로그램을 작성하시오.
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << 7 << endl;
+	cout << "hyeran9712" << endl;
+
+	return 0;
+}
