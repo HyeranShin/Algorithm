cfe42a9f4aa1432ef7070780d3372a4385b9f761
신혜란 <hyebaragi__@naver.com>
2019-04-13 19:57:08 +0900

    Create 15649. N과 M (1).cpp

---
+++ 문제집/N과 M/15649. N과 M (1).cpp
@@ -0,0 +1,47 @@
+/*
+https://www.acmicpc.net/problem/15649
+*/
+
+#include <iostream>
+#include <ios>
+#include <vector>
+using namespace std;
+
+int N, M;
+bool visited[8];
+vector<int> series;
+void dfs(int cnt);
+void print();
+int main() {
+	cin.tie(0); ios::sync_with_stdio(0);
+	cin >> N >> M;
+
+	dfs(0);
+
+	return 0;
+}
+
+void dfs(int cnt) {
+	if (cnt == M) {
+		print();
+		return;
+	}
+
+	for (int i = 0; i < N; i++) {
+		if (!visited[i]) {
+			visited[i] = true;
+			series.push_back(i + 1);
+			dfs(cnt + 1);
+			visited[i] = false;
+			series.pop_back();
+		}
+	}
+}
+
+void print() {
+	for (int i = 0; i < M; i++) {
+		cout << series[i] << ' ';
+	}
+	cout << "\n";
+
+}
