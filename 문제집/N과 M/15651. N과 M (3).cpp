4d1b8ee54f9fedeff82af43b184dcae597aacaa8
신혜란 <hyebaragi__@naver.com>
2019-04-13 20:41:16 +0900

    Create 15651. N과 M (3).cpp

---
+++ 문제집/N과 M/15651. N과 M (3).cpp
@@ -0,0 +1,39 @@
+/*
+https://www.acmicpc.net/problem/15651
+*/
+
+#include <iostream>
+#include <ios>
+#include <vector>
+using namespace std;
+
+int N, M;
+vector<int> series;
+void dfs(int cnt);
+void print();
+int main() {
+	cin.tie(0); ios::sync_with_stdio(0);
+
+	cin >> N >> M;
+	dfs(0);
+	return 0;
+}
+
+void dfs(int cnt) {
+	if (cnt == M) {
+		print();
+		return;
+	}
+	for (int i = 0; i < N; i++) {
+		series.push_back(i + 1);
+		dfs(cnt + 1);
+		series.pop_back();
+	}
+}
+
+void print() {
+	for (int i = 0; i < M; i++) {
+		cout << series[i] << ' ';
+	}
+	cout << "\n";
+}
