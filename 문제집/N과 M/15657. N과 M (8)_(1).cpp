de4a0746d04cc110e58e5e1c54932f77f89f68eb
신혜란 <hyebaragi__@naver.com>
2019-04-19 16:35:01 +0900

    Create 15657. N과 M (8)_(1).cpp

---
+++ 문제집/N과 M/15657. N과 M (8)_(1).cpp
@@ -0,0 +1,45 @@
+/*
+https://www.acmicpc.net/problem/15657
+*/
+
+#include <iostream>
+#include <ios>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M, numbers[8];
+vector<int> series;
+void dfs(int index, int cnt);
+void print();
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(0);
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		cin >> numbers[i];
+	}
+	sort(numbers, numbers + N);
+	dfs(0, 0);
+	return 0;
+}
+
+void dfs(int index, int cnt) {
+	if (cnt == M) {
+		print();
+		return;
+	}
+
+	for (int i = index; i < N; i++) {
+		series.push_back(numbers[i]);
+		dfs(i, cnt + 1);
+		series.pop_back();
+	}
+}
+
+void print() {
+	for (int i = 0; i < M; i++) {
+		cout << series[i] << ' ';
+	}
+	cout << "\n";
+}
