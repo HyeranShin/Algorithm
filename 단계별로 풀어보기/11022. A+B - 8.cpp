e4b949f50862374788ac7d0b40ae891cdd9d7e3b
신혜란 <hyebaragi__@naver.com>
2019-07-03 14:24:23 +0900

    Create 11022. A+B - 8.cpp

---
+++ 단계별로 풀어보기/11022. A+B - 8.cpp
@@ -0,0 +1,23 @@
+/*
+https://www.acmicpc.net/problem/11022
+*/
+
+#include <iostream>
+#include <ios>
+using namespace std;
+
+int T, A, B;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio();
+    
+    cin >> T;
+
+    for(int i=1; i<=T; i++) {
+        cin >> A >> B;
+        cout << "Case #" << i << ": " << A << " + " << B << " = " << A+B << "\n";
+    }
+
+    return 0;
+}
