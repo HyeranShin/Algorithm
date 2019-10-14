1f1faa535f141d266e5dcf99ecc2233c86cb0c51
신혜란 <hyebaragi__@naver.com>
2019-10-14 13:34:17 +0900

    Create 2743. 단어 길이 재기.cpp

---
+++ BOJ/2743. 단어 길이 재기.cpp
@@ -0,0 +1,19 @@
+/*
+ BOJ 2743. 단어 길이 재기
+ https://www.acmicpc.net/problem/2743
+ */
+
+#include <iostream>
+#include <ios>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(0);
+
+    string input;
+    cin >> input;
+    
+    cout << input.length();
+    
+    return 0;
+}
