62fc3b8b3e5bc48dcf4b3750f49f0988f22ba1fb
신혜란 <hyeran.dev@gmail.com>
2020-03-22 19:43:20 +0900

    add: [BOJ] 10171. 고양이

---
+++ BOJ/10171. 고양이.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/10171
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0);
+    
+    cout << "\\    /\\\n )  ( ')\n(  /  )\n \\(__)|\n";
+    
+    return 0;
+}
