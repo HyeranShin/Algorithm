4f4e3149e6e68edab5b5c2387f3c2a477beccc5e
신혜란 <hyeran.dev@gmail.com>
2020-05-01 16:16:08 +0900

    add: [BOJ] 15829. Hashing

---
+++ BOJ/15829. Hashing.cpp
@@ -0,0 +1,30 @@
+//https://www.acmicpc.net/problem/15829
+
+#include <iostream>
+#include <math.h>
+#define MOD 1234567891
+
+using namespace std;
+
+int l;
+string input;
+
+int main() {
+    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0);
+    
+    cin >> l >> input;
+    
+    long long result = 0;
+    long long temp = 1;
+    
+    for(int i=0; i<l; i++) {
+        result += ((input[i]-'a'+1)*temp)%MOD;
+        temp *= 31;
+        temp %= MOD;
+    }
+    
+    cout << result%MOD;
+    
+    return 0;
+}
